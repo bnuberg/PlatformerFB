9dcd39aa98dbae70eeea8cdd5990ec2097fb8bb0
Unknown <bnuberg@live.nl>
2019-12-14 15:48:37 +0100

    Playermovement
    Added camera follow player behaviour and player movement

---
+++ PlatformerFb/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private float delaySpeed = 2f;
+
+    [SerializeField]
+    private Vector2 offset = new Vector2(-0.7f, 1);
+
+    private GameObject playerObject;
+    private GameObject mainCameraObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerObject = GameObject.Find("Player");
+        mainCameraObject = GameObject.Find("Main Camera");
+
+        Assert.IsNotNull(playerObject);
+        Assert.IsNotNull(mainCameraObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPlayer(playerObject, mainCameraObject);
+    }
+
+    void FollowPlayer(GameObject player, GameObject mainCamera)
+    {
+        if (player != null && mainCamera != null)
+        {
+            var offsetDir = player.GetComponent<PlayerController>().GetHorizontalDirection * offset.x;
+
+            var distance = Vector2.Distance(mainCamera.transform.position, player.transform.position);
+            var xLerp = Mathf.Lerp(mainCamera.transform.position.x, player.transform.position.x + offsetDir, (delaySpeed * Time.deltaTime) / distance);
+            var yLerp = Mathf.Lerp(mainCamera.transform.position.y, player.transform.position.y + offset.y, (delaySpeed * Time.deltaTime) / distance);
+
+
+            mainCamera.transform.position = new Vector3(xLerp, yLerp, -10);
+        }
+    }
+
+}
--- PlatformerFb/Assets/Scripts/PlayerController.cs
+++ PlatformerFb/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private float movementSpeed;
+
+    private float horizontal;
+
+    public float GetHorizontalDirection { get { return horizontal; } }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        movementSpeed = 5f; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerHorizontalMovement();
     }
+
+    private void PlayerHorizontalMovement()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+
+        Vector2 position = transform.position;
+        position.x += horizontal * (movementSpeed * Time.deltaTime);
+        transform.position = position;
+    }
+
 }
