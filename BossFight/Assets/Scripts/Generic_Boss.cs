490f1db88cfa11a9f470618d31bec8689a434412
Brian <bnuberg@live.nl>
2020-03-07 17:38:54 +0100

    added boss generic class and basic UI for hp bar. TODO: HP bar implementation

--- BossFight/Assets/Scripts/Generic_Boss.cs
+++ BossFight/Assets/Scripts/Generic_Boss.cs
@@ -1,31 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Generic_Boss : MonoBehaviour
 {
+    // Health
+    [SerializeField]
+    protected Image hpBar;
+    protected int maxHealth;
+    protected int currentHealth;
 
-    private int maxHealth;
-    private int currentHealth;
+    protected int attackDamage;
 
-    private int attackDamage;
+    protected float movementSpeed;
 
-    private float movementSpeed;
+    public int getCurrentHealth { get { return currentHealth; } }
+    public int getDamage { get { return attackDamage; } }
 
-    // Start is called before the first frame update
-    void Start()
+    public Generic_Boss(int _maxHealth, int _attackDamage, float _movementSpeed)
     {
-        
+        maxHealth = _maxHealth;
+        currentHealth = _maxHealth;
+        attackDamage = _attackDamage;
+        movementSpeed = _movementSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void Attack()
     {
-        
+        // Do standard attacks stuff
     }
 
-    void Attack()
+    protected virtual void State()
     {
-
+        // Handles state of the boss
     }
+
+    protected virtual void Move()
+    {
+        // Handles movement of the boss
+    }
+
+    protected virtual void GetAbility()
+    {
+        // Ability container, get's the right ability for the state of the boss
+    }
+
 }
