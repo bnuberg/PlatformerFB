54ca27c23f47af841a0511bea71cc8fa820d6bc9
Brian <bnuberg@live.nl>
2020-03-07 14:42:19 +0100

    Generic boss vars

---
+++ BossFight/Assets/Scripts/Generic_Boss.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generic_Boss : MonoBehaviour
+{
+
+    private int maxHealth;
+    private int currentHealth;
+
+    private int attackDamage;
+
+    private float movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Attack()
+    {
+
+    }
+}
