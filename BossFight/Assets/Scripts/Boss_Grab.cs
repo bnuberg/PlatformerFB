66b2ba98d41a79065880b39039fec5473f5298d7
Brian <bnuberg@live.nl>
2020-03-22 16:37:13 +0100

    Added basic target and physics

--- BossFight/Assets/Scripts/Boss_Grab.cs
+++ BossFight/Assets/Scripts/Boss_Grab.cs
@@ -6,28 +6,47 @@ public class Boss_Grab : MonoBehaviour
 {
     [SerializeField]
     private GameObject arm;
-    private Vector2 targetPosition;
+
+    [SerializeField]
+    private float grabVelocity;
+
     private Vector2 bossPosition;
     private Quaternion bossRotation;
     private GameObject player;
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-       player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("Player");
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private Vector2 TargetPlayer()
     {
-        targetPosition = player.transform.position;
+        Vector2 targetPosition = player.transform.position;
 
         return targetPosition;
 
+    }
+
+    private void RotateGrab()
+    {
+        // Rotate arm to face player
+    }
+
+    private void UseAbility()
+    {
+        Vector2 target = TargetPlayer();
+        // Rotates to face player from boss perspective;
+        RotateGrab();
+
+        rb.velocity = target * grabVelocity;
     }
 }
